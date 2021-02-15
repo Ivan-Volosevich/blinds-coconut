e07c9f16f390df47d405235109d6796963411456
ivanvolosevich <ivan.a.volosevich@mail.ru>
2021-02-15 17:04:39 +0300

    15.02.2021

--- src/js/components/card-flip.js
+++ src/js/components/card-flip.js
@@ -4,8 +4,8 @@ function flipCard() {
     this.classList.toggle('flip');
 }
 
-// cards.forEach(card => {
-//     card.addEventListener('click', flipCard);
-// });
+cards.forEach(card => {
+    card.addEventListener('click', flipCard);
+});
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+// cards.forEach(card => card.addEventListener('click', flipCard));
