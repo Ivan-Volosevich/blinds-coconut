896e59e74d1e9c95b0347546faaadf2a745367dd
ivanvolosevich <ivan.a.volosevich@mail.ru>
2021-05-03 16:30:05 +0300

    03.05.2021

--- src/js/components/price-counter-form.js
+++ src/js/components/price-counter-form.js
@@ -1,48 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // function setCursorPosition(pos, elem) {
-    //     elem.focus();
-    //     if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
-    //     else if (elem.createTextRange) {
-    //         var range = elem.createTextRange();
-    //         range.collapse(true);
-    //         range.moveEnd("character", pos);
-    //         range.moveStart("character", pos);
-    //         range.select()
-    //     }
-    // }
+    function setCursorPosition(pos, elem) {
+        elem.focus();
+        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+        else if (elem.createTextRange) {
+            let range = elem.createTextRange();
+            range.collapse(true);
+            range.moveEnd("character", pos);
+            range.moveStart("character", pos);
+            range.select()
+        }
+    }
      
-    // function mask(event) {
-    //     var matrix = "+375 (__) ___ __ __",
-    //         i = 0,
-    //         def = matrix.replace(/\D/g, ""),
-    //         val = this.value.replace(/\D/g, "");
-    //     if (def.length >= val.length) val = def;
-    //     this.value = matrix.replace(/./g, function(a) {
-    //         return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
-    //     });
-    // };
+    function mask(event) {
+        let matrix = "+375 (__) ___ __ __",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, "");
+        if (def.length >= val.length) val = def;
+        this.value = matrix.replace(/./g, function(a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+        });
+    };
 
-    // var input = document.querySelector("#tel-form");
-    // input.addEventListener("input", mask, false);
+    let input = document.querySelector("#tel-form");
+    input.addEventListener("input", mask, false);
     
     
     
     // Get the modal
-    var modal = document.getElementById("price-modal");
+    let modal = document.getElementById("price-modal");
 
     // Get the button that opens the modal
-    var btn = document.getElementsByClassName("btn-cost")[0];
+    let btnOpenForm = document.querySelectorAll(".btn-cost");
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    let spanClose = document.querySelectorAll(".close");
 
     // When the user clicks on the button, open the modal
-    btn.onclick = function() {
+    btnOpenForm.onclick = function() {
+        console.log('---');
         modal.style.display = "block";
     }
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+    spanClose.onclick = function() {
         modal.style.display = "none";
     }
 
