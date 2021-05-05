82bf80bc9694f31636b87ea38050175cea55782c
ivanvolosevich <ivan.a.volosevich@mail.ru>
2021-05-05 03:17:25 +0300

    05.05.2021

--- src/js/components/price-counter.js
+++ src/js/components/price-counter.js
@@ -1,18 +1,9 @@
+// const { format } = require("path");
+
 window.addEventListener("DOMContentLoaded", function() {
-    // function setCursorPosition(pos, elem) {
-    //     elem.focus();
-    //     if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
-    //     else if (elem.createTextRange) {
-    //         var range = elem.createTextRange();
-    //         range.collapse(true);
-    //         range.moveEnd("character", pos);
-    //         range.moveStart("character", pos);
-    //         range.select()
-    //     }
-    // }
-     
+
     function mask(event) {
-        var matrix = "+375 (__) ___ __ __",
+        let matrix = "+375 (__) ___ __ __",
             i = 0,
             def = matrix.replace(/\D/g, ""),
             val = this.value.replace(/\D/g, "");
@@ -22,7 +13,53 @@ window.addEventListener("DOMContentLoaded", function() {
         });
     };
 
-    var input = document.querySelector("#tel");
+    const regex = /^(\+375|80)(29|25|44|33)(\d{3})(\d{2})(\d{2})$/;
+
+    const input = document.querySelector("#tel");
     input.addEventListener("input", mask, false);
+
+    const form = document.querySelector('#form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        // let error = formValidate(form);
+
+        // if (error !== 0) {
+        //     alert('Заполните номер телефона.');
+        // }
+    }
+
+    // function formValidate(form) {
+    //     let error = 0;
+    //     let formReq = document.querySelectorAll('._req');
+
+    //     for (let index = 0; index < formReq.length; index++) {
+    //         const inpuTel = formReq[index];
+    //         formRemoveError(inpuTel);
+
+    //         if (inpuTel.classList.contains('phone')) {
+    //             if (regex == false) {
+    //                 formAddError(inpuTel);
+    //                 error++;
+    //             }
+    //         } else if (inpuTel.value === "") {
+    //             formAddError(inpuTel);
+    //             error++;
+    //         }
+    //     }
+    //     return error;
+    // }
+
+    // function formAddError(inpuTel) {
+    //     inpuTel.parentElement.className.add(' error');
+    //     inpuTel.className.add(' error');
+    // }
+
+    // function formRemoveError(inpuTel) {
+    //     inpuTel.parentElement.className.replace(' error');
+    //     inpuTel.className.replace(' error');
+    // }
 });
     
