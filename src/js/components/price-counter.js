2b3cf7ec713b6b25e270970ade5e956fec0b5e20
ivanvolosevich <ivan.a.volosevich@mail.ru>
2021-05-07 10:14:01 +0300

    07.05.2021

--- src/js/components/price-counter.js
+++ src/js/components/price-counter.js
@@ -1,6 +1,41 @@
 // const { format } = require("path");
+'use strict'
 
-window.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    function setCursorPosition(pos, elem) {
+        elem.focus();
+        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+        else if (elem.createTextRange) {
+            var range = elem.createTextRange();
+            range.collapse(true);
+            range.moveEnd("character", pos);
+            range.moveStart("character", pos);
+            range.select()
+        }
+    }
+
+    function mask(event) {
+        var matrix = "+375 (__) ___ __ __",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, "");
+        if (def.length >= val.length) val = def;
+        this.value = matrix.replace(/./g, function(a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+        });
+        if (event.type == "blur") {
+            if (this.value.length == 2) this.value = ""
+        } else setCursorPosition(this.value.length, this)
+    };
+        var input = document.querySelector("#tel");
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+
+
+        
 
     // function mask(event) {
     //     let matrix = "+375 (__) ___ __ __",
@@ -13,12 +48,12 @@ window.addEventListener("DOMContentLoaded", function() {
     //     });
     // };
 
-    const regex = /^(\+375)(29|25|44|33)(\d{3})(\d{2})(\d{2})$/;
-
     // const input = document.querySelector("#tel");
     // input.addEventListener("input", mask, false);
+
+    console.log('i found it in Mask! ', input.value, '-----empty?');
     
-    const form = document.querySelector('#form');
+    const form = document.getElementById('form');
     form.addEventListener('submit', formSend);
 
     async function formSend(e) {
@@ -26,7 +61,7 @@ window.addEventListener("DOMContentLoaded", function() {
 
         let error = formValidate(form);
 
-        if (error !== 0) {
+        if (error === 0) {
             alert('Заполните номер телефона.');
         }
     }
@@ -35,31 +70,46 @@ window.addEventListener("DOMContentLoaded", function() {
         let error = 0;
         let formReq = document.querySelectorAll('._req');
 
-        for (let index = 0; index < formReq.length; index++) {
-            const input = formReq[index];
-            formRemoveError(input);
+        console.log('i found it! ', input.value);
 
-            if (input.classList.contains('phone')) {
-                if (regex == false) {
-                    formAddError(input);
+        for (let index = 0; index < formReq.length; index++) {
+            const inputTel = formReq[index];
+
+            if (inputTel.classList.contains('error')) {
+                formRemoveError(inputTel);
+            }
+
+            if (inputTel.classList.contains('phone')) {
+                if (phoneTest(inputTel)) {
+                    formAddError(inputTel);
                     error++;
                 }
-            } else if (input.value === "") {
-                formAddError(input);
-                error++;
+            } else {
+                if (inputTel.value === '') {
+                    formAddError(inputTel);
+                    error++;
+                }
             }
         }
         return error;
     }
 
-    function formAddError(input) {
-        input.parentElement.className.add(' error');
-        input.className.add(' error');
+    function formAddError(inputTel) {
+        inputTel.parentElement.classList.add('error');
+        inputTel.classList.add('error');
     }
 
-    function formRemoveError(input) {
-        input.parentElement.className.replace(' error');
-        input.className.replace(' error');
+    function formRemoveError(inputTel) {
+        inputTel.parentElement.classList.replace('error');
+        inputTel.classList.replace('error');
+    }
+
+    function phoneTest(inputTel) {
+        console.log('error in inputTel', /[\+]375\s[\(]\d{2}[\)]\s\d{3}\s\d{2}\s\d{2}/.test(inputTel.value));
+        return /[\+]375\s[\(]\d{2}[\)]\s\d{3}\s\d{2}\s\d{2}/.test(inputTel.value);
+        
+
     }
 });
-    
+
+
